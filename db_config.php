26bcb18e62a9698bda21715f5293083e72d92660
Andy James <andy@andyjames.org>
2022-04-07 22:54:16 -0500

    small changes

--- db_config.php
+++ db_config.php
@@ -1,10 +1,8 @@
 <?php
 // Create connection
-$con=mysqli_connect("127.0.0.1","root","root","loginsqli");
+$DBUSER = 'root';
+$DBPASS = 'root';
 
-// Check connection
-if (mysqli_connect_errno($con))
-  {
-  echo "<font style=\"color:#FF0000\">Could not connect:". mysqli_connect_error()."</font\>";
-  }
+$con=mysqli_connect("127.0.0.1",$DBUSER,$DBPASS,"loginsqli");
+
 ?>
--- login.php
+++ login.php
@@ -13,13 +13,13 @@ ini_set('display_errors', 1);
 
   <body>
   <div class="container-narrow">
-		
+
 		<div class="jumbotron">
 			<p class="lead" style="color:white">
 				Simple Login Page with SQL Injection
 			</p>
         </div>
-		
+
 		<div class="response">
 		<form method="POST" autocomplete="off">
 			<p style="color:white">
@@ -28,15 +28,15 @@ ini_set('display_errors', 1);
 			</p>
 			<br />
 			<p>
-			<input type="submit" value="Submit"/> 
+			<input type="submit" value="Submit"/>
 			<input type="reset" value="Reset"/>
 			</p>
 		</form>
         </div>
-    
-        
+
+
 		<br />
-		
+
       <div class="row marketing">
         <div class="col-lg-6">
 
@@ -57,26 +57,26 @@ $q = "SELECT * FROM users where username='".$username."' AND password = '".md5($
 	{
 		die('Error: ' . mysqli_error($con));
 	}
-	
+
 	$result = mysqli_query($con,$q);
 	$row_cnt = mysqli_num_rows($result);
 
 	if ($row_cnt > 0) {
-	
+
 	$row = mysqli_fetch_array($result);
-	
+
 	if ($row){
 	//$_SESSION["id"] = $row[0];
 	$_SESSION["username"] = $row[1];
 	$_SESSION["name"] = $row[3];
 	//ob_clean();
-	
-	header('Location:home.php');
+
+	header('Location:index.php');
 	}
 }
 	else{
 		echo "<font style=\"color:#FF0000\"><br \>Invalid password!</font\>";
-		
+
 	}
 }
 
@@ -85,12 +85,12 @@ $q = "SELECT * FROM users where username='".$username."' AND password = '".md5($
 
 	</div>
 	</div>
-	  
-	  
+
+
 	  <div class="footer">
 		<p>Riyaz Walikar | @riyazwalikar</p>
       </div>
 	</div> <!-- /container -->
-  
+
 </body>
 </html>
