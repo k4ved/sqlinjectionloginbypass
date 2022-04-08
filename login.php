7f97a30b6580f980d08339c7d090c93cecae2184
Andy James <andy@andyjames.org>
2022-04-07 23:30:47 -0500

    finishing templating

--- login.php
+++ login.php
@@ -5,92 +5,138 @@ include("db_config.php");
 ini_set('display_errors', 1);
 ?>
 
-<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>Simple Login Page with SQL Injection</title>
+<!DOCTYPE html>
+<html lang="en">
 
-    <link href="css/htmlstyles.css" rel="stylesheet">
-	</head>
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Majestic Admin</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="vendors/base/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="css/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="images/favicon.png" />
+</head>
 
-  <body>
-  <div class="container-narrow">
+<body>
+  <div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+      <div class="content-wrapper d-flex align-items-stretch auth auth-img-bg">
+        <div class="row flex-grow">
+          <div class="col-lg-6 d-flex align-items-center justify-content-center">
+            <div class="auth-form-transparent text-left p-3">
+              <div class="brand-logo">
+                <img src="images/logo.svg" alt="logo">
+              </div>
+              <h4>Welcome back!</h4>
+              <h6 class="font-weight-light">Happy to see you again!</h6>
+              <form class="pt-3" method="POST" autocomplete="off">
+                <div class="form-group">
+                  <label for="exampleInputEmail">Username</label>
+                  <div class="input-group">
+                    <div class="input-group-prepend bg-transparent">
+                      <span class="input-group-text bg-transparent border-right-0">
+                        <i class="mdi mdi-account-outline text-primary"></i>
+                      </span>
+                    </div>
+                    <input type="text" class="form-control form-control-lg border-left-0" name="uid" id="uid" placeholder="Username">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword">Password</label>
+                  <div class="input-group">
+                    <div class="input-group-prepend bg-transparent">
+                      <span class="input-group-text bg-transparent border-right-0">
+                        <i class="mdi mdi-lock-outline text-primary"></i>
+                      </span>
+                    </div>
+                    <input type="password" class="form-control form-control-lg border-left-0" name="password" id="password" placeholder="Password">
+                  </div>
+                </div>
+                <div class="my-2 d-flex justify-content-between align-items-center">
+                  <div class="form-check">
+                    <label class="form-check-label text-muted">
+                      <input type="checkbox" class="form-check-input">
+                      Keep me signed in
+                    </label>
+                  </div>
+                  <a href="#" class="auth-link text-black">Forgot password?</a>
+                </div>
+                <div class="my-2 d-flex justify-content-between align-items-center">
+                  <?php
 
-		<div class="jumbotron">
-			<p class="lead" style="color:white">
-				Simple Login Page with SQL Injection
-			</p>
+                  if (!empty($_GET['msg'])) {
+                      echo "<font style=\"color:#FF0000\">Please login to continue.<br\></font>";
+                  }
+                  //echo md5("pa55w0rd");
+
+                  if (!empty($_REQUEST['uid'])) {
+                  $username = ($_REQUEST['uid']);
+                  $pass = $_REQUEST['password'];
+
+                  $q = "SELECT * FROM users where username='".$username."' AND password = '".md5($pass)."'" ;
+                  //echo $q;
+                  	if (!mysqli_query($con,$q))
+                  	{
+                  		die('Error: ' . mysqli_error($con));
+                  	}
+
+                  	$result = mysqli_query($con,$q);
+                  	$row_cnt = mysqli_num_rows($result);
+
+                  	if ($row_cnt > 0) {
+
+                  	$row = mysqli_fetch_array($result);
+
+                  	if ($row){
+                  	//$_SESSION["id"] = $row[0];
+                  	$_SESSION["username"] = $row[1];
+                  	$_SESSION["name"] = $row[3];
+                  	//ob_clean();
+
+                  	header('Location:index.php');
+                  	}
+                  }
+                  	else{
+                  		echo "<font style=\"color:#FF0000\">Invalid password!</font>";
+
+                  	}
+                  }
+
+                  //}
+                  ?>
+                </div>
+                <div class="my-3">
+                  <input type="submit" class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" value="LOGIN">
+                </div>
+              </form>
+            </div>
+          </div>
+          <div class="col-lg-6 login-half-bg d-flex flex-row">
+            <p class="text-white font-weight-medium text-center flex-grow align-self-end">Copyright &copy; 2020  All rights reserved.</p>
+          </div>
         </div>
-
-		<div class="response">
-		<form method="POST" autocomplete="off">
-			<p style="color:white">
-				Username:  <input type="text" id="uid" name="uid"><br /></br />
-				Password: <input type="password" id="password" name="password">
-			</p>
-			<br />
-			<p>
-			<input type="submit" value="Submit"/>
-			<input type="reset" value="Reset"/>
-			</p>
-		</form>
-        </div>
-
-
-		<br />
-
-      <div class="row marketing">
-        <div class="col-lg-6">
-
-<?php
-
-if (!empty($_GET['msg'])) {
-    echo "<font style=\"color:#FF0000\">Please login to continue.<br\></font\>";
-}
-//echo md5("pa55w0rd");
-
-if (!empty($_REQUEST['uid'])) {
-$username = ($_REQUEST['uid']);
-$pass = $_REQUEST['password'];
-
-$q = "SELECT * FROM users where username='".$username."' AND password = '".md5($pass)."'" ;
-//echo $q;
-	if (!mysqli_query($con,$q))
-	{
-		die('Error: ' . mysqli_error($con));
-	}
-
-	$result = mysqli_query($con,$q);
-	$row_cnt = mysqli_num_rows($result);
-
-	if ($row_cnt > 0) {
-
-	$row = mysqli_fetch_array($result);
-
-	if ($row){
-	//$_SESSION["id"] = $row[0];
-	$_SESSION["username"] = $row[1];
-	$_SESSION["name"] = $row[3];
-	//ob_clean();
-
-	header('Location:index.php');
-	}
-}
-	else{
-		echo "<font style=\"color:#FF0000\"><br \>Invalid password!</font\>";
-
-	}
-}
-
-//}
-?>
-
-	</div>
-	</div>
-
-
-	  <div class="footer">
-		<p>Riyaz Walikar | @riyazwalikar</p>
       </div>
-	</div> <!-- /container -->
-
+      <!-- content-wrapper ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+  <!-- plugins:js -->
+  <script src="vendors/base/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- inject:js -->
+  <script src="js/off-canvas.js"></script>
+  <script src="js/hoverable-collapse.js"></script>
+  <script src="js/template.js"></script>
+  <!-- endinject -->
 </body>
+
 </html>
